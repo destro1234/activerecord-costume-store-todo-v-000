d07f7490df83a2422d5810f744543c795db69fcb
Elias Ventura <eliasventura23@gmail.com>
2019-01-28 13:47:13 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,12 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouse < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+
+
+    end
+  end
+
+end
